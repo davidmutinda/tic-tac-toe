24b21e2b863f2b2d575e16074247f44a7b942a09
David Kyalo <davidkymutinda@gmail.com>
2022-05-13 21:31:05 +0300

    Update tic_tac_toe.py

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,3 +1,44 @@
+def gameplay(value):
+    """This function takes the position the player inputs and prints  it on the grid"""
+    steps = [
+        (0,0),
+        (0,1),
+        (0,2),
+        (1,0),
+        (1,1),
+        (1,2),
+        (2,0),
+        (2,1),
+        (2,2)
+    ]
+    
+    try:
+        a = int(input("Choose a position (1 - 9): "))
+    except:
+        print("\033[91mInput integer values only!! \033[0m")
+        return "fail"
+    if a < 1 or a > 9:
+        print("\033[91mInput a number between 1 and 9!! \033[0m")
+        return "fail"
+    b = 1
+    for (i, j) in steps:
+        if a == b:
+            break
+        b += 1
+
+    if arr[i][j] != ' ':
+        print("\033[91mSpot occupied! Choose another position \033[0m")
+        return "fail"
+    arr[i][j] = value
+
+    
+    print(f'\033[93m{arr[0][0]} | {arr[0][1]} | {arr[0][2]}') 
+    print("---------")
+    print(f'{arr[1][0]} | {arr[1][1]} | {arr[1][2]}') 
+    print("---------")
+    print(f'{arr[2][0]} | {arr[2][1]} | {arr[2][2]}')
+    print("\033[0m")
+
 def player1():
     """This function is the entry point and allows player 1 to play it"""
     c = 0
