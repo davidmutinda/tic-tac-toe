cb20ede9f989338df7face1ca0fa9df102e0c35e
David Kyalo <davidkymutinda@gmail.com>
2022-05-13 21:30:15 +0300

    Update tic_tac_toe.py

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,3 +1,30 @@
+def player1():
+    """This function is the entry point and allows player 1 to play it"""
+    c = 0
+    d = 0
+    for c in range(0,3):
+        for d in range(0,3):
+            if arr[c][d] == " ":
+                break
+        if arr[c][d] == " ":
+            break 
+    if c == 2 and d == 2:
+        print("\033[32mDraw!! \033[0m")
+        print(" ")
+        print("\033[91mGame over!!")
+        print("\033[0m")
+        quit()
+
+    print("\033[91mPLAYER 1's turn \033[0m")
+    result = "fail"
+    while result == "fail":
+        result = gameplay('x')
+    winner = checkWinner()
+    if winner == "yes":
+        print("\033[32mPlayer 1 won!!! \033[0m")
+        return
+    player2()
+
 arr = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
 winner = " "
 print("\033[32mWELCOME TO SHELBYS' TICTACTOE GAME")
@@ -11,3 +38,4 @@ print(f'{arr[1][0]} | {arr[1][1]} | {arr[1][2]}')
 print("---------")
 print(f'{arr[2][0]} | {arr[2][1]} | {arr[2][2]}')
 print("\033[0m")
+player1()
