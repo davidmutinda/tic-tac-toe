bf7ef43f952e820d9d516e816e54029da0441eef
David Kyalo <davidkymutinda@gmail.com>
2022-05-13 21:28:49 +0300

    Update tic_tac_toe.py

---
+++ tic_tac_toe.py
@@ -0,0 +1,13 @@
+arr = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
+winner = " "
+print("\033[32mWELCOME TO SHELBYS' TICTACTOE GAME")
+print("----------------------------------")
+print("Player 1 character = x")
+print("Player 2 character = 0 \033[0m")
+print(" ")
+print(f'\033[93m{arr[0][0]} | {arr[0][1]} | {arr[0][2]}') 
+print("---------")
+print(f'{arr[1][0]} | {arr[1][1]} | {arr[1][2]}') 
+print("---------")
+print(f'{arr[2][0]} | {arr[2][1]} | {arr[2][2]}')
+print("\033[0m")
